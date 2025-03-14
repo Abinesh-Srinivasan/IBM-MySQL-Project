42b955bff25020daa4f66e52e9b7c1f9006a5b5e
Abinesh-Srinivasan <abineshsrinivasan1301@gmail.com>
2025-03-14 06:20:10 +0530

    Unwanted file deleted

--- Server Side/server.js
+++ Server Side/server.js
@@ -9,10 +9,10 @@ app.use(express.json()); // To parse JSON request body
 
 // MySQL Database Connection
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "farith", // Your MySQL root password
-  database: "ibm_project",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 // Connect to MySQL
